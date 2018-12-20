640efcf6e17c55aea55f8f6936ecf8951a1c1a43
dimmaryanto93 <software.dimas_m93@icloud.com>
2018-12-20 13:50:18 +0700

    init project

---
+++ src/main/java/com/tabeldata/training/springoauth2example/SpringOauth2ExampleApplication.java
@@ -0,0 +1,14 @@
+package com.tabeldata.training.springoauth2example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauth2ExampleApplication.class, args);
+	}
+
+}
+
