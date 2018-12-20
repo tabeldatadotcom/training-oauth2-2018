a9e314ca655a40a2bdb70587f6d21e9301169aae
dimmaryanto93 <software.dimas_m93@icloud.com>
2018-12-20 16:09:01 +0700

    enable username from database

--- src/main/java/com/tabeldata/training/springoauth2example/config/WebSecurity.java
+++ src/main/java/com/tabeldata/training/springoauth2example/config/WebSecurity.java
@@ -1,5 +1,7 @@
 package com.tabeldata.training.springoauth2example.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,9 +12,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    @Qualifier("dataSource")
+    private DataSource dataSource;
 
     @Bean
     @Override
@@ -22,16 +30,21 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(11);
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder())
-                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN", "USER")
-                .and()
-                .withUser("user").password(passwordEncoder().encode("user123")).roles("USER");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select user_id as username, password, true as enabled\n" +
+                        "from auth.users\n" +
+                        "where user_id = ?")
+                .authoritiesByUsernameQuery("select u.user_id as username, r.name as authority\n" +
+                        "from auth.users u\n" +
+                        "       join auth.user_roles ur on (u.user_id = ur.user_id)\n" +
+                        "       join auth.roles r on (r.id = ur.role_id)\n" +
+                        "where u.user_id = ?");
     }
 
     @Override
